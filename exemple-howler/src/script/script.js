37020e4b241dd513726aeca5958da79dffdf5764
Cyprian Kazimierz <cyprian.bartosik@cirvianum.cat>
2025-11-05 12:08:06 +0100

    Modificat estructira + 2 exemples llibreries JS, Fet hoewler falta Tone Cyprian

---
+++ exemple-howler/src/script/script.js
@@ -0,0 +1,24 @@
+// npm install howler
+// l'importem
+import { Howl } from "howler";
+
+var sound = new Howl({
+  src: ["/public/file_example_MP3_5MG.mp3"],
+  //Que comenci automaticament
+  //autoplay: true,
+  //Que soni en bucle
+  //loop: true,
+  //Ajustem el volu,
+  //volume: 0.5,
+});
+
+document.getElementById("btn").addEventListener("click", () => {
+  //evita que es pugi clicar molts cops el boto i que sonin tots de cop
+  sound.once("load", function () {
+    sound.play();
+  });
+});
+
+document.getElementById("btn2").addEventListener("click", () => {
+  sound.stop();
+});
